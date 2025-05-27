eb5090746b3b58d3b1a763e598c5063073002997
Victor Gominho <89556454+VictorGominho@users.noreply.github.com>
2025-05-27 19:55:00 +0000

    Nível Aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -13,6 +13,9 @@ int main() {
      
      int navio1[3] = {3, 3, 3};
      int navio2[3] = {3, 3, 3};
+     int navio3[3] = {3, 3, 3};
+     int navio4[3] = {3, 3, 3};
+
      // Colocando os navios
      for (int i = 0; i < (sizeof(navio1) / sizeof(navio1[0])); i++)
      {
@@ -30,6 +33,22 @@ int main() {
           }
      }
 
+     for (int i = 0; i < (sizeof(navio3) / sizeof(navio3[0])); i++)
+     {
+          if (tabuleiro[i][6+i] == 0)
+          {
+               tabuleiro[i][6+i] = navio3[i];
+          }
+     }
+
+     for (int i = 0; i < (sizeof(navio4) / sizeof(navio4[0])); i++)
+     {
+          if (tabuleiro[i+6][3-i] == 0)
+          {
+               tabuleiro[i+6][3-i] = navio4[i];
+          }
+     }
+
      // Mostrando o tabuleiro
      for (int i = 0; i < 10; i++)
      {
@@ -38,5 +57,7 @@ int main() {
                printf("%d ", tabuleiro[i][u]);
           }
           printf("\n");
-     }   
+     } 
+     
+     return 0;
 }
