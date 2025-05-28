cfc8dd8ec547454ed907ac93bb168a453e9cc553
Victor Gominho <89556454+VictorGominho@users.noreply.github.com>
2025-05-28 20:09:03 +0000

    Nível Mestre

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -10,13 +10,32 @@ int main() {
                tabuleiro[i][u] = 0;
           }   
      }
+
+     // Habilidades Especiais
+     int coneAoe[3][5] = {
+          {0, 0, 1, 0, 0},
+          {0, 1, 1, 1, 0},
+          {1, 1, 1, 1, 1}
+     };
+
+     int cruzAoe[3][5] = {
+          {0, 0, 1, 0, 0},
+          {1, 1, 1, 1, 1},
+          {0, 0, 1, 0, 0}
+     };
+
+     int octaAoe[3][5] = {
+          {0, 0, 1, 0, 0},
+          {0, 1, 1, 1, 0},
+          {0, 0, 1, 0, 0}
+     };
      
+     // Colocando os navios
      int navio1[3] = {3, 3, 3};
      int navio2[3] = {3, 3, 3};
      int navio3[3] = {3, 3, 3};
      int navio4[3] = {3, 3, 3};
 
-     // Colocando os navios
      for (int i = 0; i < (sizeof(navio1) / sizeof(navio1[0])); i++)
      {
           if (tabuleiro[2][i+2] == 0) // Checando se o espaço só tem água
@@ -49,6 +68,52 @@ int main() {
           }
      }
 
+     // Usando habilidades
+     // Cone
+     for (int i = 0; i < 3; i++)
+     {
+          for (int j = 0; j < 5; j++)
+          {
+               if (tabuleiro[i+1][j] == 0 && coneAoe[i][j] == 1)
+               {
+                    tabuleiro[i+1][j] = 5;
+               } else if (tabuleiro[i+1][j] == 3 && coneAoe[i][j] == 1)
+               {
+                    tabuleiro[i+1][j] = 7;
+               }  
+          }   
+     }
+
+     // Cruz
+     for (int i = 0; i < 3; i++)
+     {
+          for (int j = 0; j < 5; j++)
+          {
+               if (tabuleiro[i+2][j+5] == 0 && cruzAoe[i][j] == 1)
+               {
+                    tabuleiro[i+2][j+5] = 5;
+               } else if (tabuleiro[i+2][j+5] == 3 && cruzAoe[i][j] == 1)
+               {
+                    tabuleiro[i+2][j+5] = 7;
+               }  
+          }   
+     }
+
+     // Octaedro
+     for (int i = 0; i < 3; i++)
+     {
+          for (int j = 0; j < 5; j++)
+          {
+               if (tabuleiro[i+7][j+5] == 0 && octaAoe[i][j] == 1)
+               {
+                    tabuleiro[i+7][j+5] = 5;
+               } else if (tabuleiro[i+7][j+5] == 3 && octaAoe[i][j] == 1)
+               {
+                    tabuleiro[i+7][j+5] = 7;
+               }  
+          }   
+     }
+
      // Mostrando o tabuleiro
      for (int i = 0; i < 10; i++)
      {
@@ -58,6 +123,7 @@ int main() {
           }
           printf("\n");
      } 
+     printf("Legenda: \n 0 - Água\n 3 - Navio\n 5 - Habilidade Especial\n 7 - Navio Atingido");
      
      return 0;
 }
